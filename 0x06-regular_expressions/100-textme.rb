e2c78f75a24d3c757591f57e65b051bdd165ebb1
KamandeRick <rickmwangi237@gmail.com>
2024-03-12 13:39:45 -0700

    add capture groups

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 if ARGV.length == 1
-  sender = ARGV[0].match(/^.*\[from:(.*?)\]/).captures
+  SENDER,RECEIVER,FLAGS  = ARGV[0].match(/^.*\[from:(.*?)\].*\[to:(.*?)\].*\[flags:(.*?)\].*$/i).captures
+  puts "#{SENDER}#{RECEIVER}#{FLAGS}"
   exit
 end
