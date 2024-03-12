62c654feda5051198c98534a59dc9e46097f40a9
KamandeRick <rickmwangi237@gmail.com>
2024-03-12 13:41:32 -0700

    add commas to seperate capture groups in puts

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -2,6 +2,6 @@
 
 if ARGV.length == 1
   SENDER,RECEIVER,FLAGS  = ARGV[0].match(/^.*\[from:(.*?)\].*\[to:(.*?)\].*\[flags:(.*?)\].*$/i).captures
-  puts "#{SENDER}#{RECEIVER}#{FLAGS}"
+  puts "#{SENDER},#{RECEIVER},#{FLAGS}"
   exit
 end
