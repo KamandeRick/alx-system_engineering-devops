74bf90fa410bd32aeacb9ac11553297a8eb0754b
kamandeRick <rickmwangi237@gmail.com>
2024-09-26 18:18:58 -0700

    add file

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,14 @@
+# Add amount of traffic nginx can handle
+
+# Increase the upperlimit of default file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin'
+}
+
+#restart Nginx
+
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
